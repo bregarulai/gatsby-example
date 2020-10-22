06de3bb6c291239dcc026b22957d0478549416a9
bregarulai <bregarulai@gmail.com>
2020-10-22 13:44:33 -0400

    Used page query in graphql example page

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -2,15 +2,39 @@ import React from "react"
 import Layout from "../components/layout/Layout"
 import Header from "../components/examples/Header"
 import StaticQueryExample from "../components/examples/StaticQueryComponent"
+import { graphql } from "gatsby"
 
-const examples = () => {
+const examples = ({ data }) => {
+  const {
+    site: {
+      info: { author },
+    },
+  } = data
   return (
     <Layout>
       <h1>Hello from example page.</h1>
       <Header />
       <StaticQueryExample />
+      <h5>Author: {author}</h5>
     </Layout>
   )
 }
+
+export const data = graphql`
+  query pageQuery {
+    site {
+      info: siteMetadata {
+        title
+        description
+        author
+        data
+        person {
+          name
+          age
+        }
+      }
+    }
+  }
+`
 
 export default examples
