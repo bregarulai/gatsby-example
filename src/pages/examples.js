1817fa0ab104162b84f9e6d28e693eb07a5cbe6d
bregarulai <bregarulai@gmail.com>
2020-10-22 13:32:29 -0400

    Using StaticQuery Component in graphql

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Layout from "../components/layout/Layout"
 import Header from "../components/examples/Header"
+import StaticQueryExample from "../components/examples/StaticQueryComponent"
 
 const examples = () => {
   return (
     <Layout>
       <h1>Hello from example page.</h1>
       <Header />
+      <StaticQueryExample />
     </Layout>
   )
 }
