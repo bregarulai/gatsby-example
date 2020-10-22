eac26b675adae8f07b366c16f76f415bb94e0cfc
bregarulai <bregarulai@gmail.com>
2020-10-21 20:52:40 -0400

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Layout from "../components/layout/Layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Layout>
+        <h1>This is our home page.</h1>
+      </Layout>
+    </div>
+  )
 }
---
+++ src/pages/products.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout/Layout"
+
+const products = () => {
+  return (
+    <Layout>
+      <h1>This is out products page.</h1>
+    </Layout>
+  )
+}
+
+export default products
