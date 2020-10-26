e5d9222e43dda0cdfc5721fe2961b0463959e595
bregarulai <bregarulai@gmail.com>
2020-10-26 15:46:44 -0400

    Connected to contenful API

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require("dotenv").config()
 
 module.exports = {
   /* Your site config here */
@@ -21,6 +22,14 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-plugin-sharp`,
