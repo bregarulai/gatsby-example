08a002bd7e7ce9076731475d603b147bdd17a51c
bregarulai <bregarulai@gmail.com>
2020-10-27 01:43:28 -0400

    Created graphql query to get data from contenful

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,21 +1,23 @@
 import React from "react"
-import Layout from "../components/layout/Layout"
-import styles from "../components/products.module.css"
+import { graphql } from "gatsby"
 
-const products = () => {
-  return (
-    <Layout>
-      <div className={styles.page}>
-        <h1>This is out products page.</h1>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum deserunt
-          molestias incidunt ullam ipsa adipisci architecto laborum velit minus
-          aliquam perspiciatis quo at numquam, quasi excepturi porro quis fuga
-          assumenda?
-        </p>
-      </div>
-    </Layout>
-  )
-}
+const Products = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
 
-export default products
+export const query = graphql`
+  {
+    allContentfulProduct {
+      nodes {
+        price
+        title
+        image {
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+        slug
+      }
+    }
+  }
+`
+
+export default Products
